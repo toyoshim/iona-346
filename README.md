63f9814ebc3ee853372a0d02e473dcd9a1282707
Takashi Toyoshima <toyoshim@gmail.com>
2018-09-08 00:44:08 +0900

    v1.00
    Everything LGTM!

--- README.md
+++ README.md
@@ -1,8 +1,85 @@
-# いおな三四郎 - JVS - Sega Saturn Gamepad I/O Bridge Board by Arduino Nano
+# いおな三四郎 JVS - Sega Saturn Gamepad I/O Bridge Board
 
 This program makes an Arduino Nano work as a JVS I/O Board that allows you
 to connect a Sega Saturn Gamepad to NAOMI. Twin-Stick also works for Virtual-On.
 
-## Schematic
+Default is Twin-Stick mode. It maps as it works for Virtual-On as is.
 
-TODO
+  JVS SWs     Twin-Stick
+ 1P Start  - Start
+ 1P Up     - Left Stick Up
+ 1P Down   - Left Stick Down
+ 1P Left   - Left Stick Left
+ 1P Right  - Left Stick Right
+ 1P Push 1 - Left Trigger
+ 1P Push 2 - Left Button
+ 2P Up     - Right Stick Up
+ 2P Down   - Right Stick Down
+ 2P Left   - Right Stick Left
+ 2P Right  - Right Stick Right
+ 2P Push 1 - Right Trigger
+ 2P Push 2 - Right Button
+
+Once a combination of X and Z, or B and Y is detected, the mode will be changed
+to normal Gamepad mode. You can not make any of these combinations by Twin-Stick
+as it's right stick left and right, or up and down.
+
+  JVS SWs     Twin-Stick
+ 1P Start  - Start
+ 1P Up     - Pad Up
+ 1P Down   - Pad Down
+ 1P Left   - Pad Left
+ 1P Right  - Pad Right
+ 1P Push 1 - A
+ 1P Push 2 - B
+ 1P Push 3 - C
+ 1P Push 4 - X
+ 1P Push 5 - Y
+ 1P Push 6 - Z
+ 1P Push 7 - L
+ 1P Push 8 - R
+
+## Schematic
+```
+
+Arduino Nano
+
+                         o TX1  VIN o--------------o 5V
+JVS Data+ o--------------o RX0  GND o
+                         o RST  RST o
+                         o GND   5V o
+JVS Data- o--------------o D2    A7 o
+JVS Sense o---o---VVVV---o D3    A6 o
+              |  100 Ohm
+       100 nF =
+              |
+            -----
+            /////
+  Coin SW o--------------o D4    A5 o--------------o Saturn d3
+          o--------------o D5    A4 o--------------o Saturn d2
+          o--------------o D6    A3 o--------------o Saturn d1
+          o--------------o D7    A2 o--------------o Saturn d0
+          o--------------o D8    A1 o--------------o Saturn s1
+          o--------------o D9    A0 o--------------o Saturn s0
+          o--------------o D10 AREF o--------------o
+          o--------------o D11  3V3 o
+          o--------------o D12  D13 o--------------o LED on Nano
+
+
+JVS - Series B connector (same with USB Type B)
+  ________
+ /        \
+ | 1#  #2 |  1 Sense
+ |  +--+  |  2 Data-
+ |  +--+  |  3 Data+
+ | 4#  #3 |  4 GND
+ +--------+
+
+
+Sega Saturn Gamepad IO (female - host side)
+ _____________________________
+/                             \ 1 GND  4 5V   7 d0
+| 9# 8# 7# 6# 5# 4# 3# 2# 1#  | 2 d2   5 s1   8 d1
+| =========================== | 3 d3   6 s0   9 5V
++-----------------------------+
+```
---
+++ iona-346.ino
@@ -0,0 +1,228 @@
+// Copyright 2018 Takashi Toyoshima <toyoshim@gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "jvsio/JVSIO.cpp"
+
+// PIO pins used by JVSIO
+//  RX0 - JVS Data+
+//  D2  - JVS Data-
+//  D3  - JVS Sense
+//  D13 - LED
+
+// PIO pins used by 346
+//  A0  - SS s0 [out]
+//  A1  - SS s1 [out]
+//  A2  - SS d0 [in]
+//  A3  - SS d1 [in]
+//  A4  - SS d2 [in]
+//  A5  - SS d3 [in]
+//  D4  - Coin  [in]
+
+JVSIO io;
+
+static const char gamepad_id[] = "SEGA ENTERPRISES,LTD.compat;IONA-346-NANO;ver1.00;Gamepad Mode";
+static const char twinstick_id[] = "SEGA ENTERPRISES,LTD.compat;IONA-346-NANO;Ver1.00;Twin-Stick Mode";
+
+uint8_t coin_count = 0;
+uint8_t coin = 0;
+uint16_t pad = 0xffff;
+
+bool twinstick_mode = true;
+
+void strobe(int s) {
+  digitalWrite(A0, (s & 1) ? HIGH : LOW);
+  digitalWrite(A1, (s & 2) ? HIGH : LOW);
+}
+
+int peek() {
+  return (
+    (digitalRead(A2) << 0) |
+    (digitalRead(A3) << 1) |
+    (digitalRead(A4) << 2) |
+    (digitalRead(A5) << 3));
+}
+
+void updateState() {
+  // Update coin.
+  uint8_t new_coin = digitalRead(4);
+  if (coin && !new_coin)
+    coin_count++;
+  coin = new_coin;
+
+  // Update pad state.
+  //    F  E  D  C  B  A  9  8  7  6  5  4  3  2  1  0  bit/mode
+  //  [ R  X  Y  Z  S  A  C  B  >  <  v  ^  L  1  0  0] gamepad
+  //  [LB R< R^ R>  S RT RB Rv L> L< Lv L^ LT  1  0  0] twinstick
+  strobe(0);
+  pad = peek() << 12;
+  strobe(1);
+  pad |= peek() << 8;
+  strobe(2);
+  pad |= peek() << 4;
+  strobe(3);
+  pad |= peek();
+
+  // Update mode.
+  // - disconnected, reset to default
+  if ((pad & 0x0007) != 4)
+    twinstick_mode = true;
+  // - impossible stick combination results in gamepad mode
+  if (((pad & 0x5000) == 0) || ((pad & 0x2100) == 0))
+    twinstick_mode = false;
+}
+
+uint8_t report(size_t player, size_t line) {
+  if (twinstick_mode) {
+    //       |  B7 |  B6 |  B5 |  B4 |  B3 |  B2 |  B1 |  B0 |
+    // ------+-----+-----+-----+-----+-----+-----+-----+-----+
+    // P0-L1 |Start|  -  | L U | L D | L L | L R |L sht|L trb|
+    // P0-L2 |  -  |  -  |  -  |  -  |  -  |  -  |  -  |  -  |
+    // P1-L1 |  -  |  -  | R U | R D | R L | R R |R sht|R trb|
+    // P1-L2 |  -  |  -  |  -  |  -  |  -  |  -  |  -  |  -  |
+    if (player > 1 || line != 1)
+      return 0x00;
+    if (player == 0) {
+      return ~(0x40 |
+          ((pad & 0x0800) >>  4) |
+          ((pad & 0x0010) <<  1) |
+          ((pad & 0x0020) >>  1) |
+          ((pad & 0x0040) >>  3) |
+          ((pad & 0x0080) >>  5) |
+          ((pad & 0x0008) >>  2) |
+          ((pad & 0x8000) >> 15));
+    } else {
+      return ~(0xc0 |
+          ((pad & 0x2000) >>  8) |
+          ((pad & 0x0100) >>  4) |
+          ((pad & 0x4000) >> 11) |
+          ((pad & 0x1000) >> 10) |
+          ((pad & 0x0400) >>  9) |
+          ((pad & 0x0200) >>  9));
+    }
+  } else {
+    //       |  B7 |  B6 |  B5 |  B4 |  B3 |  B2 |  B1 |  B0 |
+    // ------+-----+-----+-----+-----+-----+-----+-----+-----+
+    // P0-L1 |Start|  -  | Up  |Down |Left |Right|  A  |  B  |
+    // P0-L2 |  C  |  X  |  Y  |  Z  |  L  |  R  |  -  |  -  |
+    // P1-L1 |  -  |  -  |  -  |  -  |  -  |  -  |  -  |  -  |
+    // P1-L2 |  -  |  -  |  -  |  -  |  -  |  -  |  -  |  -  |
+    if (player != 0 || line > 2)
+      return 0x00;
+    if (line == 1) {
+      return ~(0x40 |
+          ((pad & 0x0800) >>  4) |
+          ((pad & 0x0010) <<  1) |
+          ((pad & 0x0020) >>  1) |
+          ((pad & 0x0040) >>  3) |
+          ((pad & 0x0080) >>  5) |
+          ((pad & 0x0400) >>  9) |
+          ((pad & 0x0100) >>  8));
+    } else {
+      return ~(0x03 |
+          ((pad & 0x0200) >>  2) |
+          ((pad & 0x4000) >>  8) |
+          ((pad & 0x2000) >>  8) |
+          ((pad & 0x1000) >>  8) |
+          ((pad & 0x0008) >>  0) |
+          ((pad & 0x8000) >> 13));
+    }
+  }
+}
+
+void setup() {
+  io.begin();
+
+  // Initialize output pins.
+  {
+    const int out_pins[] = { A0, A1 };
+    for (size_t i = 0; i < sizeof(out_pins); ++i)
+      pinMode(out_pins[i], OUTPUT);
+  }
+  strobe(0);
+
+  // Initialize input pins.
+  {
+    const int in_pins[] = { 4, A2, A3, A4, A5 };
+    for (size_t i = 0; i < sizeof(in_pins); ++i)
+      pinMode(in_pins[i], INPUT_PULLUP);
+  }
+}
+
+void loop() {
+  uint8_t len;
+  uint8_t* data = io.getNextCommand(&len);
+  if (!data) {
+    updateState();
+    return;
+  }
+  switch (*data) {
+   case JVSIO::kCmdIoId:
+    io.pushReport(JVSIO::kReportOk);
+    {
+      const char* id = twinstick_mode ? twinstick_id : gamepad_id;
+      for (size_t i = 0; id[i]; ++i)
+        io.pushReport(id[i]);
+    }
+    io.pushReport(0);
+
+    // Initialize.
+    coin_count = 0;
+    break;
+   case JVSIO::kCmdFunctionCheck:
+    io.pushReport(JVSIO::kReportOk);
+
+    io.pushReport(0x01);  // sw
+    io.pushReport(0x02);  // players
+    io.pushReport(0x0C);  // buttons
+    io.pushReport(0x00);
+
+    io.pushReport(0x03);  // analog inputs
+    io.pushReport(0x08);  // channels
+    io.pushReport(0x00);  // bits
+    io.pushReport(0x00);
+
+    io.pushReport(0x12);  // general purpose driver
+    io.pushReport(0x08);  // slots
+    io.pushReport(0x00);
+    io.pushReport(0x00);
+
+    io.pushReport(0x02);  // coin
+    io.pushReport(0x02);  // slots
+    io.pushReport(0x00);
+    io.pushReport(0x00);
+
+    io.pushReport(0x00);
+    break;
+   case JVSIO::kCmdSwInput:
+    io.pushReport(JVSIO::kReportOk);
+    io.pushReport(0x00);  // TEST, TILT1-3, and undefined x4.
+    for (size_t player = 0; player < data[1]; ++player) {
+      for (size_t line = 1; line <= data[2]; ++line)
+        io.pushReport(report(player, line));
+    }
+    break;
+   case JVSIO::kCmdCoinInput:
+    io.pushReport(JVSIO::kReportOk);
+    for (size_t slot = 0; slot < data[1]; ++slot) {
+      io.pushReport((0 << 6) | 0);
+      io.pushReport(slot ? 0x00 : coin_count);
+    }
+    break;
+   case JVSIO::kCmdAnalogInput:
+    io.pushReport(JVSIO::kReportOk);
+    for (size_t channel = 0; channel < data[1]; ++channel) {
+      io.pushReport(0x80);
+      io.pushReport(0x00);
+    }
+    break;
+   case JVSIO::kCmdCoinSub:
+    if (!data[1])
+      coin_count -= data[3];
+    io.pushReport(JVSIO::kReportOk);
+    break;
+   case JVSIO::kCmdDriverOutput:
+    io.pushReport(JVSIO::kReportOk);
+    break;
+  }
+}
